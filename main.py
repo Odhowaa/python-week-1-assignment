d2555bb9bb7230c4f17677f1d19514507ec9b9c8
Ahmed Ibrahim Odhowaa <ahmedodhowaa781@gmail.com>
2026-09-23 12:08:37 +0300

    Add Week 1 All About Me program

---
+++ main.py
@@ -0,0 +1,5 @@
+
+print("My name is", "Ahmed Ibrahim Odhowaa")
+print("My age is", 19, "years old")
+print("My favourite programming language is", "Python")
+print("My goal for this course is", "to become a skilled Software Developer")
